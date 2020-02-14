fad2ab3370abea6ddf25313ae36d4f49b7d8cf1e
Nalina Madhav C <nalinamadhavc@Nalinas-MacBook-Pro.local>
2020-02-14 17:39:51 +0530

    Initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
+import ProductCard from 'flight-deals'
 
-import ExampleComponent from 'flight-deals'
 
 export default class App extends Component {
   render () {
+     const flights= [
+      {
+          id: 10,
+          image:'./assets/images/destinations/goa.jpg',
+          location: 'Goa',
+          date:'Sun, 16 Feb ',
+          price:' 1,569 INR '
+      
+      },
+      {
+          id: 11,
+          image:'./assets/images/destinations/delhi.jpg',
+          location: 'New Delhi',
+          date:'Sun, 16 Feb ',
+          price:' 3,230 INR '
+      
+      },
+     
+      {
+          id: 12,
+          image:'./assets/images/destinations/kochi.jpg',
+          location: 'Kochi',
+          date:'Sun, 16 Feb ',
+          price:' 2,054 INR '
+      
+      },
+      {
+          id: 13,
+          image:'./assets/images/destinations/mumbai.jpg',
+          location: 'Mumbai',
+          date:'Sun, 16 Feb ',
+          price:' 1,800 INR '
+      
+      }
+  ]
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ProductCard title="Flight promo to Asia and beyond" subTitle="Seats are limited. Book now before they're all gone." location={flights}></ProductCard>
+        
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-
 import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+export default class ProductCard extends Component {
+  static PropTypes = {
+    title: PropTypes.string,
+    subTitle: PropTypes.string,
+    location: PropTypes.arrayOf(PropTypes.object).isRequired
   }
-
   render() {
-    const {
-      text
-    } = this.props
-
+    const locations = this.props.location.map(location => {
+      return (
+        <div key={location.id} className={styles.griditems}>
+          <div inline className={styles.imageitem}>
+            <img className={styles.imgtag} alt={location.location} src={location.image} />
+          </div>
+          <div className={styles.textitem}>
+            <h6>{location.location}</h6>
+            <p className={styles.date} >{location.date}</p>
+            <p className={styles.price} >{location.price}</p>
+          </div>
+        </div>
+      )
+    }
+    )
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div className={styles.gridcontainer}>
+        <div className={styles.gridtitle}>
+          <h3 >{this.props.title}</h3>
+          <h6 >{this.props.subTitle}</h6>
+        </div>
+        <div className={styles.gridproductcard}>
+          {locations}
+        </div>
       </div>
     )
   }
